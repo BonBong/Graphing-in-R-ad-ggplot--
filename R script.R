85f5a46c4031f333ef60ce59ad96e6a580b482e9
Bongani JekyllHydes Mveng <bonbong93@yahoo.co.za>
2016-06-24 03:46:25 +0200

    nothing to say

---
+++ R script.R
@@ -0,0 +1,42 @@
+sample(colors(), 10)
+pal <- colorRamp(c("red", "blue"))
+pal(0)
+1
+3
+pal(1)
+3
+skip()
+skip()
+1
+1
+1
+pal(seq(0, 1, len = 6)
+    skip()
+    
+    
+    p1 <- colorRampPalette(c("red", "blue"))
+    p1(2)
+    p1(6)
+    line(0xcc)
+    0xcc
+    swirl()
+    info()
+    main()
+    1
+    info()
+    main()
+    2
+    1
+    8
+    
+    
+    
+    4
+    1
+    2
+    
+    
+  
+    ?str
+    str(mpg)
+    qplot
